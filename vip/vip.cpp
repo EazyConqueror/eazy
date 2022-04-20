3571cd7e2376510346135bdf48baf831175699f6
EazyConqueror <96054158+EazyConqueror@users.noreply.github.com>
2022-04-20 21:49:47 +0300

    Update vip.cpp

--- vip/vip.cpp
+++ vip/vip.cpp
@@ -1,110 +1,58 @@
 proxies:
-  - name: "trojan"
-    type: trojan
-    server: s.eazyconqueror.tk
-    port: 8443
-    password: f42f0327-0b5d-4c93-a36d-dabcfb765153
-    alpn:
-      - h2
-      - http/1.1
-    skip-cert-verify: true
-    
-  - name: "vmess"
-    type: vmess
-    server: s.eazyconqueror.tk
-    port: 95
-    uuid: 4185b610-c69b-408c-aea7-bbec1f525b8c
-    alterId: 0
-    cipher: auto
+  - name: mr.loby.ml3
+    server: 15.185.233.7
+    port: 43401
+    type: ss
+    cipher: chacha20-ietf-poly1305
+    password: t3tcjh6LVgRg
     udp: true
-    skip-cert-verify: true
-    network: ws
-    ws-opts:
-      path: /v2rayws
-      headers:
-        Host: s.eazyconqueror.tk
-    
-  - name: "vmess-grpc"
-    server: s.eazyconqueror.tk
-    port: 2099
-    type: vmess
-    uuid: f89beb4c-83e7-4ff8-a3cf-483bcfd9de45
-    alterId: 0
-    cipher: auto
-    network: grpc
-    tls: true
-    servername: s.eazyconqueror.tk
-    udp: true
-    skip-cert-verify: true
-    grpc-opts:
-      grpc-service-name: "scvps"
-      
-  - name: "ssr"
-    type: ssr
-    server: 207.154.225.199
-    port: 1443
-    udp: true
-    cipher: aes-256-cfb
-    password: vip3
-    obfs: tls1.2_ticket_auth
-    protocol: auth_chain_a
-    # obfs-param: domain.tld
-    # protocol-param: "#"
-    # udp: true
 proxy-groups:
-  - name: gameTROJAN
+  - name: mrloby
     type: url-test
     url: http://www.gstatic.com/generate_204
     interval: 300
     tolerance: 50
     proxies:
-      - "trojan"
-  - name: gameWS
-    type: select
-    proxies:
-      - "vmess"
-  - name: gameGRPC
-    type: select
-    proxies:
-      - "vmess-grpc"
-  - name: gameSSR
-    type: select
-    proxies:
-      - "ssr"
-  - name: gameDIR
+      - mr.loby.ml3
+  - name: game
     type: select
     proxies:
       - DIRECT
 rules:
-  - DOMAIN,www.pubgmobile.com,gameTROJAN
-  - DOMAIN,dl.listdl.com,gameTROJAN
-  - DOMAIN,app-measurement.com,gameTROJAN
-  - DOMAIN,lh3.googleusercontent.com,gameTROJAN
-  - DOMAIN,android.crashsight.wetest.net,gameTROJAN
-  - DOMAIN-SUFFIX,gstatic.com,gameTROJAN
-  - DOMAIN-SUFFIX,gcloudcs.com,gameTROJAN
-  - DOMAIN-SUFFIX,sg.tdatamaster.com,gameTROJAN
-  - DOMAIN-SUFFIX,proximabeta.com,gameTROJAN
-  - DOMAIN-SUFFIX,twimg.com,gameTROJAN
-  - DOMAIN-SUFFIX,fbsbx.com,gameTROJAN
-  - DOMAIN-SUFFIX,facebook.com,gameTROJAN
-  - DOMAIN-SUFFIX,adjust.com,gameTROJAN
-  - DOMAIN-SUFFIX,amsoveasea.com,gameTROJAN
-  - DOMAIN-SUFFIX,gcloudsdk.com,gameTROJAN
-  - DOMAIN-SUFFIX,gjacky.com,gameTROJAN
-  - DOMAIN-SUFFIX,anticheatexpert.com,gameTROJAN
-  - DOMAIN-SUFFIX,onezapp.com,gameTROJAN
-  - DOMAIN-SUFFIX,qq.com,gameTROJAN
-  - DOMAIN-SUFFIX,qcloud.com,gameTROJAN
-  - DOMAIN-SUFFIX,doubleclick.com,gameTROJAN
-  - DOMAIN-SUFFIX,googleapis.com,gameTROJAN
-  - GEOIP,CN,gameTROJAN
-  - DST-PORT,9031,gameDIR
-  - DST-PORT,20001,gameDIR
-  - DST-PORT,20000,gameDIR
-  - DST-PORT,20002,gameDIR
-  - DOMAIN-SUFFIX,igamecj.com,gameDIR
-  - IP-CIDR,162.0.0.0/8,gameWS
-  - IP-CIDR,49.0.0.0/8,gameWS
-  - IP-CIDR,20.0.0.0/8,gameWS
+  - DOMAIN,www.pubgmobile.com,mrloby
+  - DOMAIN,dl.listdl.com,mrloby
+  - DOMAIN,app-measurement.com,mrloby
+  - DOMAIN,lh3.googleusercontent.com,mrloby
+  - DOMAIN,android.crashsight.wetest.net,mrloby
+  - DOMAIN-SUFFIX,gstatic.com,mrloby
+  - DOMAIN-SUFFIX,gcloudcs.com,mrloby
+  - DOMAIN-SUFFIX,sg.tdatamaster.com,mrloby
+  - DOMAIN-SUFFIX,proximabeta.com,mrloby
+  - DOMAIN-SUFFIX,twimg.com,mrloby
+  - DOMAIN-SUFFIX,fbsbx.com,mrloby
+  - DOMAIN-SUFFIX,facebook.com,mrloby
+  - DOMAIN-SUFFIX,adjust.com,mrloby
+  - DOMAIN-SUFFIX,amsoveasea.com,mrloby
+  - DOMAIN-SUFFIX,gcloudsdk.com,mrloby
+  - DOMAIN-SUFFIX,gjacky.com,mrloby
+  - DOMAIN-SUFFIX,anticheatexpert.com,mrloby
+  - DOMAIN-SUFFIX,onezapp.com,mrloby
+  - DOMAIN-SUFFIX,qq.com,mrloby
+  - DOMAIN-SUFFIX,qcloud.com,mrloby
+  - DOMAIN-SUFFIX,doubleclick.com,mrloby
+  - DOMAIN-SUFFIX,googleapis.com,mrloby
+  - DST-PORT,9031,mrloby
+  - IP-CIDR,203.205.239.243/32,mrloby
+  - IP-CIDR,58.251.106.185/32,mrloby
+  - GEOIP,CN,mrloby
+  - DOMAIN,naping.igamecj.com,game
+  - DOMAIN,saping.igamecj.com,game
+  - DOMAIN,krping.igamecj.com,game
+  - DOMAIN,hkping.igamecj.com,game
+  - DOMAIN,meping.igamecj.com,game
+  - DOMAIN,euping.igamecj.com,game
+  - DOMAIN-SUFFIX,igamecj.com,mrloby
+  - IP-CIDR,162.0.0.0/8,game
+  - IP-CIDR,49.0.0.0/8,game
+  - IP-CIDR,20.0.0.0/8,game
   - MATCH,DIRECT
