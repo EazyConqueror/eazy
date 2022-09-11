c53b9be0e5f42ceff8e7daf77242ea27df668d57
EazyConqueror <96054158+EazyConqueror@users.noreply.github.com>
2022-09-11 14:43:13 +0300

    Update vip.cpp

--- vip/vip.cpp
+++ vip/vip.cpp
@@ -3,7 +3,7 @@ proxies:
     type: vmess
     server: it1.vpnjantit.com
     port: 10000
-    uuid: da59b982-ffa6-11ec-bfa0-371440e8e0e4
+    uuid: 64f80246-31c6-11ed-8208-6340be144b72
     alterId: 0
     cipher: auto
     #udp: true
@@ -24,45 +24,16 @@ proxy-groups:
     url: 'http://www.gstatic.com/generate_204'
     interval: 300
 rules:
-  - DST-PORT,9030,gameTLS
-  - DST-PORT,9031,gameTLS
-  - DST-PORT,20000,gameTLS
-  - DST-PORT,20001,gameTLS
-  - DST-PORT,20002,gameTLS
-  - DOMAIN,www.pubgmobile.com,gameTLS
-  - DOMAIN,dl.listdl.com,gameTLS
-  - DOMAIN,crl3.digicert.com,gameTLS
-  - DOMAIN,www.microvirt.com,gameTLS
-  - DOMAIN,android-safebrowsing.google.com,gameTLS
-  - DOMAIN,app-measurement.com,gameTLS
-  - DOMAIN,lh3.googleusercontent.com,gameTLS
-  - DOMAIN,android.crashsight.wetest.net,gameTLS
-  - DOMAIN-SUFFIX,gstatic.com,gameTLS
-  - DOMAIN-SUFFIX,gcloudcs.com,gameTLS
-  - DOMAIN-SUFFIX,sg.tdatamaster.com,gameTLS
-  - DOMAIN-SUFFIX,proximabeta.com,gameTLS
-  - DOMAIN-SUFFIX,twimg.com,gameTLS
-  - DOMAIN-SUFFIX,fbsbx.com,gameTLS
-  - DOMAIN,graph.facebook.com,gameTLS
-  - DOMAIN-SUFFIX,adjust.com,gameTLS
-  - DOMAIN-SUFFIX,amsoveasea.com,gameTLS
-  - DOMAIN-SUFFIX,gcloudsdk.com,gameTLS
-  - DOMAIN-SUFFIX,gjacky.com,gameTLS
-  - DOMAIN-SUFFIX,anticheatexpert.com,gameTLS
-  - DOMAIN-SUFFIX,onezapp.com,gameTLS
-  - DOMAIN-SUFFIX,qcloud.com,gameTLS
-  - DOMAIN-SUFFIX,doubleclick.com,gameTLS
-  - DOMAIN-SUFFIX,googleapis.com,gameTLS
-  - DOMAIN-SUFFIX,gvt1.com,gameTLS
-  - DOMAIN-SUFFIX,igamecj.com,gameTLS
-  - DOMAIN-SUFFIX,qq.com,gameTLS
-  - DOMAIN-SUFFIX,gcloudcs.com,gameTLS
-  - IP-CIDR,203.205.239.243/24,gameTLS
-  - IP-CIDR,129.226.2.165/24,gameTLS
-  - IP-CIDR,172.16.0.1/24,gameTLS
-  - IP-CIDR,137.208.72.137/24,gameTLS
-  - IP-CIDR,162.0.0.0/8,gameTLS
-  - IP-CIDR,49.0.0.0/8,gameTLS
-  - IP-CIDR,20.0.0.0/8,gameTLS
-  - GEOIP,CN,gameTLS
-  - MATCH,DIRECT
+  - DOMAIN-SUFFIX,version.common.freefiremobile.com,gameTLS
+  - DOMAIN-SUFFIX,csoversea.castle.freefiremobile.com,gameTLS
+  - DOMAIN-SUFFIX,client.common.freefiremobile.com,gameTLS
+  - DOMAIN-SUFFIX,d.network.freefiremobile.com,gameTLS
+  - DOMAIN-SUFFIX,freefiremobile-a.akamaihd.net,gameTLS
+  - DOMAIN-SUFFIX,id.event.freefiremobile.com,gameTLS
+  - DOMAIN-SUFFIX,dl.wsu.freefiremobile.com,gameTLS
+  - DOMAIN-SUFFIX,login.common.freefiremobile.com,gameTLS
+  - DOMAIN-SUFFIX,bling2.midasplayer.com,gameTLS
+  - DOMAIN-SUFFIX,dl.castle.freefiremobile.com,gameTLS
+  - DOMAIN-SUFFIX,ff.garena.com,gameTLS
+  - DOMAIN-SUFFIX,garena.com,gameTLS
+  - MATCH,gameTLS
